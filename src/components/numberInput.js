d008b67f41addb1ad124788592c7057ee1794c91
Inseo B <yunibaek@hotmail.com>
2017-07-07 22:25:10 -0400

    number input: add logic for input field

---
+++ src/components/numberInput.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+// Input field
+class numberInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.validate = this.validate.bind(this);
+    this.change = this.change.bind(this);
+
+    //initial state
+    this.state = {
+      validation: "Correct 10-digit number",
+      value: props.value
+    };
+  }
+
+  // validate whether user inputed data is correct or false
+  validate() {
+    if (this.props.validate) {
+      const validation = this.props.validate.call(this, this.state.value);
+      this.setState({ validation: validation });
+    }
+  }
+
+  //set state to user inputed value on the input field change
+  change({ target: { value } }) {
+    if (this.props.onChange) {
+      this.props.onChange.apply(this, arguments);
+    }
+
+    this.setState({ value: value });
+  }
+
+  // value becomes props
+  componentWillReceiveProps({ value }) {
+    this.setState({ value: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          className="ft-input"
+          value={this.props.value}
+          onBlur={this.validate}
+          onChange={this.change}
+        />
+        {this.state.validation &&
+          <div className={"error"}>{this.state.validation}</div>}
+      </div>
+    );
+  }
+}
+
+export default numberInput;
